115f3ab118d7e716dce4c4e49096dafdb628af2d
NELLY CASTILLO CADENAS <99703789+Nelly-Castillo@users.noreply.github.com>
2024-09-21 18:51:06 -0600

    Changes: Bd and ORM (v21)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EmployeesModule } from './employees/employees.module';
 import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [EmployeesModule, ProductsModule],
+  imports: [
+    ConfigModule.forRoot(), 
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.HOST,
+      port: +process.env.PORT,
+      username: 'postgres',
+      password: process.env.PASS,
+      database: process.env.NAME,
+      entities: [],
+      synchronize: true,
+    }),
+    EmployeesModule,
+    ProductsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
