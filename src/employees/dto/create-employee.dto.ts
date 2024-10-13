515a1ea9bb59b5b2a456304c1eafd5b5a0d6ad77
NELLY CASTILLO CADENAS <99703789+Nelly-Castillo@users.noreply.github.com>
2024-10-12 19:41:50 -0600

    Changes: FindByProvider and routess(26v)

--- src/employees/dto/create-employee.dto.ts
+++ src/employees/dto/create-employee.dto.ts
@@ -1,5 +1,5 @@
 import { IsEmail, IsString, MaxLength } from "class-validator";
-
+import { Employee } from '../entities/employee.entity';
 export class CreateEmployeeDto {
     @IsString()
     @MaxLength(30)
@@ -10,6 +10,7 @@ export class CreateEmployeeDto {
     @IsString()
     @MaxLength(10)
     phoneNumber: string;
+
     @IsString()
     @IsEmail()
     email:string;
